5f0abc35b1b13a4588782c3f906636108a2ddb28
Nicolas Abadia Pinheiro <62776448+Nicolas-Abadia@users.noreply.github.com>
2024-02-05 03:00:45 +0000

    Gallery Zoom

---
+++ javascript/script.js
@@ -0,0 +1,30 @@
+document.querySelectorAll('.column img').forEach(image => {
+    image.onclick = () =>{
+        // Obtém o elemento com a classe .popup-img e o mesmo id da imagem clicada
+        let popup = document.querySelector(`.popup-img#${image.id}`);
+
+        // Se encontrou um elemento correspondente, exibe ele e esconde os outros
+        if (popup) {
+            popup.style.display = 'flex';
+            popup.querySelector('img').src = image.getAttribute('src');
+            document.querySelectorAll('.popup-img').forEach(p => {
+                if (p !== popup) {
+                    p.style.display = 'none';
+                }
+            });
+        }
+        document.querySelector('body').style.overflow = 'hidden';
+        
+    }
+    
+});
+document.querySelectorAll('.popup-img span').forEach(span => {
+    span.onclick = () => {
+        let popup = span.closest('.popup-img');
+        // Esconde esse elemento
+        popup.style.display = 'none';
+        span.parentElement.style.display = 'none';
+        
+        document.querySelector('body').style.overflow = 'auto';
+    }
+});
